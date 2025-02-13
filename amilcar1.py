7767021addcef44b8f0e3b3f281cc17051b9ad8e
Amilcar Armmand <armmand@gmail.com>
2025-02-13 16:07:08 -0500

    Updated to track moves and cooperation rate. Todo: defection rate.

--- amilcar1.py
+++ amilcar1.py
@@ -5,19 +5,26 @@ from participant import Move, Prisoner
 class Amilcar1(Prisoner):
     def __init__(self):
         super().__init__()
-        self.cooperation = 0.1
+        self.coop_probability = 0.50
         self.their_history = []
+        self.game_count = 0
+        self.opponent_name = None
 
     @classmethod
     def get_name(self) -> str:
         return "Amilcar1"
 
     def play(self) -> Move:
-        if random.random() < 0.1:
+        if self.their_history:
+            self.coop_rate = sum(1 for move in self.their_history if move == Move.COOPERATE) / len(self.their_history)
+
+            self.coop_probability = 0.2 + self.coop_rate * 0.9
+        self.game_count += 1
+        if random.random() < self.coop_probability:
             return Move.COOPERATE
         else:
             return Move.DEFECT
 
     def result(self, your_move: Move, their_move: Move, payout: int,
                their_payout: int) -> None:
-        ...
+        self.their_history.append(their_move)
